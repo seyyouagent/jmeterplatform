51c9e976f92f6bb80afcdf6eadcefc11f0bdcd52
liuhuan13 <liuhuan13@meicai.cn>
2020-11-25 09:45:28 +0800

    update utils

--- src/main/java/com/test/mapper/CompanyMapper.java
+++ src/main/java/com/test/mapper/CompanyMapper.java
@@ -1,7 +1,6 @@
 package com.test.mapper;
 
 import com.test.bean.entity.Company;
-import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 
--- src/main/java/com/test/service/CompanyService.java
+++ src/main/java/com/test/service/CompanyService.java
@@ -10,6 +10,7 @@ import java.util.List;
 @Service
 public class CompanyService {
 
+
     @Autowired
     CompanyMapper companyMapper;
 
